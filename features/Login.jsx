1c7c448ae8d749b578ac4668786bbb864effeb09
shantel1111 <148761452+shantel1111@users.noreply.github.com>
2026-03-26 00:12:22 +0100

    Sign up functionality

--- features/Login.jsx
+++ features/Login.jsx
@@ -1,12 +1,14 @@
 import "../styles/login.css";
+import React from "react";
 
 export default function Login({ setLogin }) {
+  const [isSignUp, setSignUp] = React.useState(false);
   return (
     <>
       <section className="form-container">
         <div>
           <h1>filmflow.</h1>
-
+          <h2>{isSignUp ? "SIGN UP" : "LOGIN"}</h2>
           <form>
             <label htmlFor="email">
               email:
@@ -16,6 +18,17 @@ export default function Login({ setLogin }) {
               pswrd:
               <input id="password" type="password" name="password" required />
             </label>
+            {isSignUp && (
+              <label htmlFor="confirm-password">
+                confirm pswrd:
+                <input
+                  id="confirm-password"
+                  type="password"
+                  name="confirm-password"
+                  required
+                />
+              </label>
+            )}
             <button
               type="submit"
               onClick={() => {
@@ -25,6 +38,16 @@ export default function Login({ setLogin }) {
               submit
             </button>
           </form>
+          <p>
+            Not a member yet?{" "}
+            <button
+              onClick={() => {
+                setSignUp((prevVal) => !prevVal);
+              }}
+            >
+              {isSignUp ? "LOGIN" : "SIGN UP"}
+            </button>
+          </p>
         </div>
       </section>
     </>
